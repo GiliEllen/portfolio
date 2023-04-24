5d35f349700524e035510e571cdaab828857f5ad
Gili <gilimena95@gmail.com>
2023-04-24 12:36:09 +0300

    WIP

--- src/components/projects/projectCard/ProjectCard.tsx
+++ src/components/projects/projectCard/ProjectCard.tsx
@@ -1,47 +1,44 @@
 import React, { FC, useEffect, useState } from "react";
 import { Project } from "../../../information/projects";
-import github from "../../../assets/images/github.svg"
+import github from "../../../assets/images/github.svg";
 
 interface ProjectCardProps {
-  project: Project
+  project: Project;
   idx: number;
-
 }
 
-const ProjectCard: FC<ProjectCardProps> = ({project, idx}) => {
-  const [dir, setDir] = useState<string>();
-  const [src, setSrc] = useState<string>()
+const ProjectCard: FC<ProjectCardProps> = ({ project, idx }) => {
+  const [dir, setDir] = useState<any>("row");
+  const [src, setSrc] = useState<string>();
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
   useEffect(() => {
     if (idx % 2 === 0) {
-      setDir("left");
+      setDir("row");
     } else {
-      setDir("right");
+      setDir("row-reverse");
     }
-    setSrc(project.image)
+    if (windowSize < 500) {
+      setDir("column");
+    }
+    setSrc(project.image);
   }, []);
 
   function handleHover() {
-    setSrc(project.hover)
+    setSrc(project.hover);
   }
 
   function handleLeave() {
-    setSrc(project.image)
+    setSrc(project.image);
   }
 
   return (
-    <div
-      className="project"
-      style={
-        dir === "left"
-          ? { flexDirection: "row"}
-          : { flexDirection: "row-reverse" }
-      }
-    >
-      <div className="project__main" style={
-        dir === "left"
-          ? { paddingRight: "37px"}
-          : { paddingLeft: "25px" }
-      }>
+    <div className="project" style={{ flexDirection: dir }}>
+      <div
+        className="project__main"
+        style={
+          dir === "left" ? { paddingRight: "37px" } : { paddingLeft: "25px" }
+        }
+      >
         <h2 className="project__main__feature">feature project</h2>
         <h1 className="project__main__header">{project.header}</h1>
         <p className="project__main__info">{project.info}</p>
@@ -50,10 +47,20 @@ const ProjectCard: FC<ProjectCardProps> = ({project, idx}) => {
             return <p>{technology}</p>;
           })}
         </div>
-        <a href={project.link}><button className="btn-project"><img src={github} alt="github logo"/>Github</button></a>
+        <a href={project.link}>
+          <button className="btn-project">
+            <img src={github} alt="github logo" />
+            Github
+          </button>
+        </a>
       </div>
-      <div className="project__image-container" >
-        <img src={src} alt={project.header} onMouseOver={handleHover} onMouseLeave={handleLeave}/>
+      <div className="project__image-container">
+        <img
+          src={src}
+          alt={project.header}
+          onMouseOver={handleHover}
+          onMouseLeave={handleLeave}
+        />
       </div>
     </div>
   );
